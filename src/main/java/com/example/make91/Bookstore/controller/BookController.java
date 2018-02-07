b31e81dec72a0c2d1b47344b303eff425b677c11
make91 <make91@gmail.com>
2018-02-07 16:20:36 +0200

    REST

--- src/main/java/com/example/make91/Bookstore/controller/BookController.java
+++ src/main/java/com/example/make91/Bookstore/controller/BookController.java
@@ -1,11 +1,15 @@
 package com.example.make91.Bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.make91.Bookstore.model.Book;
 import com.example.make91.Bookstore.model.BookRepository;
@@ -15,16 +19,31 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 
-	@RequestMapping(value = { "/index", "/booklist", "/" })
+	@RequestMapping(value = { "/index", "/booklist", "/"})
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findOne(bookId);
+    }
 
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		return "addbook";
+	}
+	
+	@RequestMapping(value = "/books", method = RequestMethod.POST)
+	public @ResponseBody Book addBookRest(@RequestBody Book book) {
+    	return repository.save(book);
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)

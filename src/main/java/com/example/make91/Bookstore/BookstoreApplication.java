ffea6467509c25b583a2a056db92c79b1564ea57
Marcus Kivi <make91@gmail.com>
2018-03-18 22:34:29 +0000

    persistent database (mysql)

--- src/main/java/com/example/make91/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/make91/Bookstore/BookstoreApplication.java
@@ -20,24 +20,36 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner demo(BookRepository brepository, UserRepository urepository) {
 		return (args) -> {
-			brepository.save(new Book("The Girl on the Train", " Paula Hawkins", 2015, "9781594633669", 10));
-			brepository.save(new Book("Into the Water", " Paula Hawkins", 2017, "9780735211209", 20));
+			// database must exist before running (mysql>create database bookstore character set utf8;)
+			System.out.println("==========BOOKS IN LIST===========");
+			int i = 0;
+			for (Book book : brepository.findAll()) {
+				i++;
+			}
+			System.out.println(i);
+			// only add books if there are none
+			if (i == 0) {
+				System.out.println("===ZERO BOOKS, ADDING TWO BOOKS===");
+				brepository.save(new Book("The Girl on the Train", " Paula Hawkins", 2015, "9781594633669", 10));
+				brepository.save(new Book("Into the Water", " Paula Hawkins", 2017, "9780735211209", 20));
+			}
 			System.out.println("===========LIST OF BOOKS==============");
 			for (Book book : brepository.findAll()) {
 				System.out.println(book.toString());
 			}
-			System.out.println("=============BOOK WITH ID=1===========");
-			System.out.println(brepository.findOne(1L).toString());
-			System.out.println("======================================");
-			
 			// Create users: make91/asdf1 admin/Admin1234!
 			User user1 = new User("make91", "$2a$10$v3Yqcay/P7.w0kEVcVvJ/uCObI8QcDNMxSEKkzvmTBJyOtdvTBjl6", "make91@m.com", "USER");
 			User user2 = new User("admin", "$2a$10$Fciq2HqH/zJuGVMxX0dTHeKcV1zUa2izRu0Og1jzx3LyXdY4fLG06", "admin@m.com", "ADMIN");
-			urepository.save(user1);
-			urepository.save(user2);
+			// avoid exception from adding same user twice
+			try {
+				urepository.save(user1);
+				urepository.save(user2);
+			} catch (Exception e) {}
+			System.out.println("=================USERS================");
 			for (User user : urepository.findAll()) {
-				System.out.println("Added user " + user.getUsername());
+				System.out.println(user.getUsername());
 			}
+			System.out.println("======================================");
 		};
 	}
 }
